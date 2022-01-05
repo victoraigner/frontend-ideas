0922a628a8fb493dcea938f1c6fa4397ede0cdde
Victor Aigner <victor@aigner.uy>
2022-01-05 15:04:05 -0300

    Added MovieApp

---
+++ MovieApp/script.js
@@ -0,0 +1,63 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=df894646a61a17b0eca04db152f9f90c&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=df894646a61a17b0eca04db152f9f90c&query="';
+
+const form = document.getElementById("form");
+
+//get movies to show initially
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByVote(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+
+    `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByVote(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return red;
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_URL + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
